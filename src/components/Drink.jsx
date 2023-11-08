b4d84a9f4a1f6111eee53049bd3110dcb2e0be69
gabicilfone <cilfonegabriel@gmail.com>
2023-11-08 16:17:17 -0300

    Add component Card to the drink

--- src/components/Drink.jsx
+++ src/components/Drink.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import { Col, Card, Button } from "react-bootstrap"
 
 const Drink = ({drink}) => {
   return (
-    <div>
-      {drink.strDrink}
-    </div>
+    <Col md={6} lg={3}>
+        <Card className="mb-4">
+            <Card.Img 
+                variant="top"
+                src={drink.strDrinkThumb}
+                alt={`Imagen de ${drink.strDrink}`}
+            />
+
+            <Card.Body>
+                <Card.Title>
+                    {drink.strDrink}
+                </Card.Title>
+
+                <Button
+                    variant="warning"
+                    className="w-100 text-uppercase mt-2"
+                >
+                    See Recipe
+                </Button>
+            </Card.Body>
+        </Card>
+    </Col>
   )
 }
 
